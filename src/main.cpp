c051b9348d2a1c2f972156f21cbc0064db4b9717
Arnau Espejo <arnaueh@gmail.com>
2021-05-16 23:46:51 +0200

    CMake test

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-BAR-4-1-indiestudio-arnau.espejo-hernandez
+** File description:
+** Created by aespejo,
+*/
+
+#include "scene.hpp"
+
+int main()
+{
+    const int width = 800;
+    const int height = 450;
+    InitWindow(width, height, "\0");
+    SetTargetFPS(60);
+    Scene main_scene;
+    main_scene.setCameraPosition((Vector3) { 0.0f, 10.0f, 10.0f });
+
+    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        BeginMode3D(main_scene.getCamera());
+        DrawCube(cubePosition, 2, 2, 2, RED);
+        DrawCubeWires(cubePosition, 2, 2, 2, MAROON);
+        DrawGrid(10, 1);
+        DrawFPS(10, 10);
+        EndMode3D();
+        EndDrawing();
+    }
+    CloseWindow();
+}
