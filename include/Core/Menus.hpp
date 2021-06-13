50cf40a56b3fb8d27769d13653f8ab27afde9ed6
kocariz <kattalin.ocariz-monedero@epitech.eu>
2021-06-13 20:55:47 +0200

    feat: menus enum separated

---
+++ include/Core/Menus.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** Menus.hpp.h
+** File description:
+** Menus.hpp.h
+*/
+#ifndef BOMBERMAN_MENUS_HPP
+#define BOMBERMAN_MENUS_HPP
+
+enum Menu {
+    MAIN = 0,
+    MID,
+    GAME,
+    GAME_OVER,
+    EXIT,
+    SETTINGS
+};
+
+#endif //BOMBERMAN_MENUS_HPP
