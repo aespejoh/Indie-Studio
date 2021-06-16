b3eb4717cabbc3b8f02a0f65cc8aed020c624ec7
kocariz <kattalin.ocariz-monedero@epitech.eu>
2021-06-16 17:58:23 +0200

    fix: mid menu working, still have to finish to fix background and return of player and color selected

--- include/Core/Menus/Mid.hpp
+++ include/Core/Menus/Mid.hpp
@@ -12,15 +12,24 @@
 
 #define WIDTH 1000
 #define HEIGHT 750
+#define NUM_FRAMES 2
 
 class Mid {
     public:
         explicit Mid(Core *core);
         ~Mid();
         Menu menu();
-        void loadTexture();
+        void loadModels();
+        void loadTextures();
+        void loadRect();
         void drawings();
+        void drawModels();
+        void drawPlayersInfo();
+        void checkMouse();
         void setPositions();
+        void drawButton();
+        void playMouseCheck();
+        void initInfo();
     private:
         Core *core;
         Model boomberman_blue;
@@ -33,11 +42,25 @@ class Mid {
         Texture2D red;
         Texture2D arrow_left;
         Texture2D arrow_right;
+        Texture2D playButton;
+        Texture2D background;
         Vector3 position;
         Vector3 position_two;
         Vector3 position_three;
         Vector3 position_four;
         Font font;
+        Vector2 mousePoint = { 0.0f, 0.0f };
+        bool arrow_one = false;
+        bool arrow_two = false;
+        Rectangle arrow_left_one_rect = {0, 0, 0, 0};
+        Rectangle arrow_right_one_rect = {0, 0, 0, 0};
+        Rectangle arrow_left_two_rect = {0, 0, 0, 0};
+        Rectangle arrow_right_two_rect = {0, 0, 0, 0};
+        float playFrameHeight = 0.0;
+        Rectangle playBtnBounds = {0, 0, 0, 0};
+        Rectangle playSourceRec = {0, 0, 0, 0};
+        int playState = 0;  // Button state: 0-NORMAL, 1-MOUSE_HOVER, 2-PRESSED
+        bool playAction = false;
 };
 
 #endif /* !MID_HPP_ */
