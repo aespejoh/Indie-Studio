824f1e9e0a5ab7922dbafc06afa88378cb3aa00f
Alvaro <alvaro.garcia-bamala@epitech.eu>
2021-06-18 01:57:49 +0200

    [Updated] RenderModule to renderMap using DrawCube() (no textures added for the moment)

--- include/Engine/Objects/renderModule.hpp
+++ include/Engine/Objects/renderModule.hpp
@@ -8,13 +8,15 @@
 #define INDIE_STUDIO_RENDERMODULE_HPP
 
 #include "MsgNode.hpp"
+#include <MapModule/mapModule.h>
 #include <raylib.h>
 
 class RenderModule : public MsgNode {
     public:
         explicit RenderModule(MessageBus *bus);
         void update() override;
-        void renderMap();
+        void renderMap(MapModule &map);
+
     protected:
         void renderMapTile(Vector3 position);
         void onNotify(Msg message) override;
--- src/Engine/RenderModule/renderModule.cpp
+++ src/Engine/RenderModule/renderModule.cpp
@@ -21,12 +21,18 @@ void RenderModule::onNotify(Msg message)
 
 }
 
-void RenderModule::renderMap()
-{
-
-}
-
 void RenderModule::renderMapTile(Vector3 position)
 {
     DrawCube(position, 1, 1, 1, BLACK);
+}
+
+void RenderModule::renderMap(MapModule &map)
+{
+    std::vector<Vector3> boxes = map.getPositions().at("boxes");
+    std::vector<Vector3> walls = map.getPositions().at("walls");
+
+    for (auto & boxPosition : boxes)
+        DrawCube(boxPosition, 1.0f, 1.0f, 1.0f, BLUE);
+    for (auto & colPosition : walls)
+        DrawCube(colPosition, 1.0f, 1.0f, 1.0f, GREEN);
 }
